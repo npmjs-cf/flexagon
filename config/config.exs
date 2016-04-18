b81b5c9f04b9688471c4a61957f7120f76038d8a
Terin Stock <terinjokes@gmail.com>
2016-04-18 15:41:15 -0700

    chore(config): document the configuration options

--- config/config.exs
+++ config/config.exs
@@ -1,31 +1,18 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+# Flexagon Configuration File
 use Mix.Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
+# Flexagon currently exposes three toggles in configuration:
+#  - target: the main npm registry
+#  - scopeTarget: the npm registry used to scoped packages
+#  - port: the HTTP port to listen for requests on
+#
+#  If this project is started directly, mix will load the configuration
+#  from this file. Otherwise, mix will load the configuration from
+#  *your* application.
 
-# You can configure for your application as:
-#
-#     config :flexagon, key: :value
-#
-# And access this configuration in your application as:
-#
-#     Application.get_env(:flexagon, :key)
-#
-# Or configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
+config :flexagon, target: "registry.npmjs.cf"
+config :flexagon, scopeTarget: "registry.npmjs.com"
+config :flexagon, port: 4001
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
-
+# The logger's default configuration doesn't include the Request Id
+config :logger, :console, metadata: [:request_id]
